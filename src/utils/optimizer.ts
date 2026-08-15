e318ef6d9a3c24646ef73ca2b5de3b216b909569
Senuka Wijerathna <senukaw615@gmail.com>
2026-08-16 00:07:10 +0800

    Add missing comments to the split option in the algorithm and the optimisation summary options.

--- src/utils/optimizer.ts
+++ src/utils/optimizer.ts
@@ -98,20 +98,28 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
     // Second option: Check all 2 store pairs.
     const splitResults: SplitStoreResult[]=[];
 
+    // Go through each available store.
     for (let i=0; i< stores.length;i++) {
+
+        // Go through each store pair for current store.
         for (let j =i+1; j<stores.length;j++) {
             const storeA=stores[i];
             const storeB =stores[j];
 
+            // Total items cost
             let rawItemsCost=0;
+
+            // Avilable items for each store and items missing in both of them.
             const itemsFromA: SplitStoreResult['itemsFromA']=[]
             const itemsFromB: SplitStoreResult['itemsFromB']=[]
             const missingItems: string[]=[];
 
+            // Go through each item in the basket and check their availability and price comparisons.
             for (const item of basket) {
                 const priceA=storeA.inventory[item.name];
                 const priceB=storeB.inventory[item.name];
 
+                // Case where item exists in both stores, take the cheaper store.
                 if (priceA!==undefined && priceB !== undefined) {
                     if (priceA<=priceB){
                         itemsFromA.push({
@@ -130,6 +138,7 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
                         });
                         rawItemsCost+=priceB*item.quantity;
                     }
+                // Case where only store A has the item, take store A.
                 } else if (priceA!==undefined) {
                     itemsFromA.push({
                             name: item.name,
@@ -138,6 +147,8 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
                             total: priceA * item.quantity
                         });
                         rawItemsCost+=priceA*item.quantity;
+                
+                // Case where only store B has the item, choose store B.
                 } else if (priceB!==undefined) {
                     itemsFromB.push({
                             name: item.name,
@@ -146,19 +157,28 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
                             total: priceB * item.quantity
                         });
                         rawItemsCost+=priceB*item.quantity;
+                
+                // Case where both stores doesn't have the item, add it to the missing list.
                 } else {
                     missingItems.push(item.name);
                 }
             }
 
+            // Checks if there is an actual split between two stores.
             const isActualSplit=itemsFromA.length>0 && itemsFromB.length>0;
+
+            // No penalty if all the items are from a single store.
             const actualPenalty=isActualSplit? tripPenalty:0;
+
+            // Actual total cost including the trip penalty.
             const effectiveTotalCost=rawItemsCost+actualPenalty;
 
+            // Determine the baseline cost to calculate savings. If a single store can fulfill the order, use its cost, else use the raw items cost.
             const singleBaseline=bestSingle?bestSingle.totalCost:rawItemsCost;
             const grossSavings=singleBaseline-rawItemsCost;
             const netSavings=singleBaseline-effectiveTotalCost;
 
+            // Add the split pairs to the array.
             splitResults.push({
                 storeA: storeA.name,
                 storeB: storeB.name,
@@ -175,11 +195,13 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
         }
     }
 
-    const validSplits=splitResults.filter((s)=>s.isFullyFulfilled && s.itemsFromA.length > 0 && s.itemsFromB.length > 0)
-                                .sort((a,b)=>a.effectiveTotalCost-b.effectiveTotalCost);
+    // Get the valid splits ()
+    const validSplits=splitResults.filter((s)=>s.isFullyFulfilled && s.itemsFromA.length > 0 && s.itemsFromB.length > 0).sort((a,b)=>a.effectiveTotalCost-b.effectiveTotalCost);
     const bestSplit=validSplits.length>0? validSplits[0]:null;
 
     // Create the Recommendation Summary
+
+    // Case where none of the two options provides all the items.
     if(!bestSingle && !bestSplit) {
         return {
             bestSingleStore: null,
@@ -189,7 +211,10 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
         };
     }
 
+    // Case where both options can provide all the items.
     if (bestSplit && bestSingle) {
+
+        // If there is a save from the best split.
         if (bestSplit.netSavings>0) {
             return {
                 bestSingleStore: bestSingle,
@@ -206,6 +231,8 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
             };
         }
     }
+
+    // Case where there is a best split option but no single store option.
     if (bestSplit && !bestSingle) {
         return {
             bestSingleStore: null,
@@ -214,6 +241,8 @@ export function optimizeBasket(basket: BasketItem[], stores: Store[],tripPenalty
             summaryReason: 'No single store has all items. Split between ${bestSplit.storeA} and ${bestSplit.storeB} to fulfill the basket for RM ${bestSplit.effectiveTotalCost.toFixed(2)} total.'
         };
     }
+
+    // Case where there is a best single store option but no split option.
     return {
         bestSingleStore: bestSingle,
         bestSplit: null,
